0fd57f918f56c7728bdbd28205cf142f17c1f64d
ivan006 <ivan.copeland2015@gmail.com>
2020-06-24 14:34:06 +0200

    1

---
+++ laravel/tests/Browser/RegisterTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RegisterTest extends DuskTestCase
+{
+  use DatabaseMigrations;
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function a_user_can_register()
+    {
+        $this->browse(function (Browser $browser) {
+          $browser->visit('/register')
+          ->assertSee('Register')
+          ->type('name', 'Ivan')
+          ->type('email', 'ivan.copeland2015@gmail.com')
+          ->type('password', 'secret123')
+          ->type('password_confirmation', 'secret123')
+          ->press('Register')
+          ->assertPathIs('/home');
+        });
+    }
+}
