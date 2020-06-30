3dd6c53eabba0fe1b366013587ee16c6546c5a59
ivan006 <ivan.copeland2015@gmail.com>
2020-06-30 10:19:33 +0200

    1

--- tests/Browser/duskSpiderTest.php
+++ tests/Browser/duskSpiderTest.php
@@ -6,18 +6,21 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
+use App\Page;
+use Facebook\WebDriver\WebDriverBy;
+
 class duskSpiderTest extends DuskTestCase
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
-        });
-    }
+    // /**
+    //  * A Dusk test example.
+    //  *
+    //  * @return void
+    //  */
+    // public function testExample()
+    // {
+    //     $this->browse(function (Browser $browser) {
+    //         $browser->visit('/')
+    //                 ->assertSee('Laravel');
+    //     });
+    // }
 }
