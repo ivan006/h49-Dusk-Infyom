331a3702dd5fc7fc6d11dc24a029d997bc0b8499
ivan006 <ivan.copeland2015@gmail.com>
2020-07-07 13:30:51 +0200

    1

---
+++ app/test.php
@@ -0,0 +1,151 @@
+22seven
+adsoftware
+atk
+activate commerce
+adams & associates - strategy. systems. skills
+adbot
+advocate | attorney
+aerobotics
+altron (bytes and altech)
+appointli (pty) ltd.
+appy - building beautiful software
+attitude dynamics
+bee mobile
+bsg
+besidecode (pty) ltd
+betflash (pty) ltd
+blue apple payroll & blue apple time south africa
+blue gheko
+blue yonder, inc.
+bluesky
+bridge learning
+brightmind studios
+bytes universal systems (altech isis)
+crmbill - web based business management
+chouette software solutions
+cloudsmiths cape town
+codeforté systems (pty) ltd
+codebus.io - app & software development company
+cogent (pty) ltd
+complexus (pty) ltd
+compucore
+custom software development
+data dada
+dataplait (pty) ltd
+datasmith
+devman - is kidz africa
+digirisk (pty) ltd
+digiata technology services (pty) ltd
+drive revenue
+erply africa
+erpweb
+esp salon & spa software
+esp salon and spa software
+epaypro
+free airtime in south africa
+fullstack technology
+firepay
+flat circle
+flat circle
+gridview
+grove | applied intelligence
+hr software
+healthcent pty ltd
+hipzone
+hybeam networks (pty) ltd
+hyrax biosciences
+ict-works
+ixd database solutions
+infront
+internet solutions (is)
+iplex technologies
+linc communications
+link
+linum labs
+linx
+liquid thought
+listio
+litmusdigital
+lucid ocean
+lucid ocean pty (ltd)
+mohara ventures sa
+made code
+magnetic software
+nvoice online invoicing
+nanoson
+netstar
+nite owls
+nitric industries
+nuvalaw
+onboard technologies
+offerzen
+p2 group
+plugged in web
+pageman digital signage software
+palmetrics (pty) ltd
+pinpoll gmbh
+pixel project
+play it safe
+ploiter
+property accounting system
+prospa
+proudly associated (pty) ltd
+purple muffin productions
+r-data
+r-data (pty) ltd
+rategain technologies ltd
+react technology
+reactore systems
+remind works
+replenish group
+retail and restaurant business systems
+roomraccoon | hotel management software
+sacotech
+sap
+skyros-ci
+sqli pty ltd
+supremacy international
+sacotech diamond auctions technologies limited
+sayhi
+securebux
+senix digital
+shop ahead
+simigenix
+siteminder south africa
+siyelo
+smartreporting (pty) ltd
+soarsoft international
+software colors (pty) ltd
+stadium
+supernice
+supply chain junction
+supply chain junction (pty) ltd
+synapseon
+tct digital pty ltd
+tabit (pty) ltd
+techno ponies (pty) ltd
+telesis interactive
+the delta studio
+thefactory
+think tank software solutions
+uber greenlight
+urban-hub
+virtual mobile technologies
+voxeye
+vsoftsystems
+wala
+webdash digital | we build exceptional web and mobile apps
+whereismytransport
+wow fingers
+xentient
+yellowtail software
+zailab
+brandowle
+data visioner
+digiwave
+graylink
+hellohr
+io
+rooster
+sharecapitalwealth
+tctdigital
